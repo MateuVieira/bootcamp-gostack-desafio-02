6d5b2fd4102cc54331afc9def905e1c3f00aa99b
MateuVieira <mateus12v@hotmail.com>
2020-01-30 17:41:35 -0300

    Add auth Middleware

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import autgMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// All routes after authMiddleware will be need a token
+routes.use(autgMiddleware);
 
 routes.put('/users', UserController.update);
 
