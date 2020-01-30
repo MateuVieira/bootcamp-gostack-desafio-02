c53ad535dbd2f495a01b85d53bcbfa58b6539a8a
MateuVieira <mateus12v@hotmail.com>
2020-01-30 18:06:41 -0300

    Update UserController update Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -34,6 +34,23 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      password: Yup.string()
+        .min(6)
+        .when('old_password', (old_password, field) =>
+          old_password ? field.required() : field
+        ),
+      confirm_password: Yup.string('passowrd', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation faild' });
+    }
+
     const { email, old_password } = req.body;
 
     const user = await User.findByPk(req.userId);
